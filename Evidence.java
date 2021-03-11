2bd4b446816eefda83179fe7b5535a3f8d411d9e
Austin Stipo <astipo@email.sc.edu>
2021-03-11 11:23:30 -0500

    made Evidence capital

---
+++ Evidence.java
@@ -0,0 +1,75 @@
+/**
+ * @author Sarah Carlucci 
+ */
+import java.util.ArrayList;
+
+public class Evidence {
+    
+    private String evidenceType;
+    private ArrayList<String> locations;
+    private ArrayList<String> times;
+    private User enteredEvidence;
+    private ArrayList<User> handleEvidence;
+    private ArrayList<String> comments;
+
+
+    public Evidence(String evidenceType, String location, String time, User enteredEvidence, String comment) {
+
+    }
+
+    public String getEvidenceType() {
+        return "";
+    } 
+
+    public void addLocation(String location) {
+
+    } 
+
+    public ArrayList<String> getLocations() {
+        return locations;
+    } 
+
+    public String getLastLocation() {
+        return "";
+    } 
+
+    public void addTime(String time) {
+
+    } 
+
+    public ArrayList<String> getTimes() {
+        return times;
+    } 
+
+    public String getLastTime() {
+        return "";
+    } 
+
+    public User getEnteredEvidence() {
+        return handleEvidence.get(0);
+    } 
+
+    public void addHandleEvidence(User user) {
+
+    } 
+
+    public  ArrayList<User> getHandleEvidence() {
+        return handleEvidence;
+    }
+
+    public User getLastHandleEvidence() {
+        return handleEvidence.get(0);
+    }
+
+    public void addComment(String comment) {
+
+    }
+
+    public ArrayList<String> getComments() {
+        return comments;
+    } 
+
+    public String getLastComment() {
+        return "";
+    }
+}
