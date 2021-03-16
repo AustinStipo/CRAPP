5fc843c82c89ce2c7c1bfcb2cab294e849f99638
alexsantoro26 <77907258+alexsantoro26@users.noreply.github.com>
2021-03-15 21:55:44 -0400

    Update PersonOfInterest.java

--- PersonOfInterest.java
+++ PersonOfInterest.java
@@ -13,46 +13,50 @@ public class PersonOfInterest extends Person{
     private ArrayList<String> stories;
 
     public PersonOfInterest(UUID id, String firstName, String lastName, int age, int height, int weight, String skinColor, String hairColor, String eyeColor, String address, String phoneNumber, String email, String associationToCase, String associationToSuspect) {
-
+        super(id, firstName, lastName, age, height, weight, skinColor, hairColor, eyeColor, address);
+        setPhoneNumber(phoneNumber);
+        setEmail(email);
+        setAssociationToSuspect(associationToSuspect);
+        setSuspectAssociatedTo(suspectAssociatedTo);
     }
 
     public String getPhoneNumber() {
-        return "";
+        return this.phoneNumber;
     } 
 
     public void setPhoneNumber(String phoneNumber) {
-
+        this.phoneNumber = phoneNumber;
     } 
 
     public String getEmail() {
-        return "";
+        return this.email;
     } 
 
     public void setEmail(String email) {
-
+        this.email = email
     }
 
     public String getAssociationToSuspect() {
-        return "";
+        return this.associationToSuspect
     } 
 
-    public void setAssociationToSuspect(String association) {
-
+    public void setAssociationToSuspect(String associationToSuspect) {
+        this.associtationToSuspect = associationToSuspect;
     }
 
     public String getSuspectAssociatedTo() {
-        return "";
+        return this.associtationToSuspect;
     }
 
     public void setSuspectAssociatedTo(String association) {
-
+        this.suspectAssociatedTo = association;
     }
 
     public void addStory(String story) {
-
+        stories.add(story);
     } 
 
     public  String getLastStory() {
-        return "";
+        return stories.get(stories.size()-1);
     }
 }
