e7107993c376489048025bfcbc9f54437bf7d199
Connor Downey <connordowney6@gmail.com>
2021-03-24 11:40:47 -0400

    some frontend work

--- CrappUI.java
+++ CrappUI.java
@@ -23,12 +23,74 @@ public class CrappUI {
      */
     public void run() {
         System.out.println(WELCOME_MESSAGE);
-        //register or login (maybe make a new method to return the correct next page)
-        //home page after login w/ multiple options
-            //new method with switch statement
-            //1. add a crime
-            //2. add a criminal
-            //3. 
+
+        loginScreen();
+        
+    }
+
+    /**
+     * Prints the login/register screen
+     * @return 
+     */
+    public boolean loginScreen() {
+        System.out.println("1. Login");
+        System.out.println("2. Register");
+        int logOrReg = promptUser(2);
+        if(logOrReg == 1)
+        {
+            return login();
+        }
+        else if(logOrReg == 2)
+        {
+            register();
+            return login();
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void register() {
+        System.out.println("Enter your First Name: ");
+        String firstName = scanner.nextLine();
+        System.out.println("Enter your Last Name: ");
+        String lastName = scanner.nextLine();
+        System.out.println("Enter your Badge Number: ");
+        String email = scanner.nextLine();
+        System.out.println("Enter your Email: ");
+        String phoneNumber = scanner.nextLine();
+        System.out.println("Enter your Phone Number: ");
+        String precinct = scanner.nextLine();
+        System.out.println("Enter your Precinct: ");
+        String department = scanner.nextLine();
+        System.out.println("Enter your Department: ");
+        String badgeNumber = scanner.nextLine();
+        System.out.println("Enter your Badge Number: ");
+        String rank = scanner.nextLine();
+        System.out.println("Enter your Rank: ");
+        String password = scanner.nextLine();
+        //TODO fix this
+        int id = 10;
+        User user = new User(id, firstName, lastName, email, phoneNumber, precinct, department, badgeNumber, rank, password, null);
+        crapp.addUser(user);
+    }
+
+    /**
+     * The login for the user
+     * @return Returns true if the login is successful and false otherwise
+     */
+    public boolean login() {
+        System.out.println("Enter your Badge Number: ");
+        String badgeNumber = scanner.nextLine();
+        System.out.println("Enter your Password: ");
+        String password = scanner.nextLine();
+        for(User user : crapp.getUsers()) {
+            if(user.getBadgeNumber().equals(badgeNumber) && user.getPassword().equals(password)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
@@ -36,6 +98,10 @@ public class CrappUI {
      */
     public void mainMenu() {
         System.out.println("******** MAIN MENU ********");
+        for(int i=0;i<mainMenuOptions.length;i++) {
+            System.out.println((i+1) + ". " + mainMenuOptions[i]);
+        }
+        System.out.println("\n");
     }
 
     /**
