3d794c94b2d88b2302a6c69eb8970da53f561eb6
Connor Downey <connordowney6@gmail.com>
2021-03-24 16:21:32 -0400

    finished main menu

--- CrappUI.java
+++ CrappUI.java
@@ -6,9 +6,15 @@ import java.util.Scanner;
 public class CrappUI {
 
     private static final String WELCOME_MESSAGE = "Welcome to CRAPP";
-	private String[] mainMenuOptions = {};
+	private String[] mainMenuOptions = {"MAIN MENU", "Make an Addition", "Make a Removal", "Search the Database", "Logout"};
+    private String[] loginOptions = {"LOGIN OR REGISTER", "Login", "Register"};
+    private String[] additionOptions = {"ADD TO THE DATABASE", "Add Crime", "Add Criminal", "Add Suspect", "Add Witness", "Add Person of Interest", "Add Victim"};
+    private String[] removalOptions = {"REMOVE FROM THE DATABASE", "Remove Crime", "Remove Criminal", "Remove Suspect", "Remove Witness", "Remove Person of Interest", "Remove Victim"};
+    private String[] searchOptions = {"SEARCH", "Search for a Crime", "Search for a Criminal"};
 	private Scanner scanner;
 	private CriminalApplication crapp;
+    private boolean loginAttempt = false;
+    private boolean loggedOut = false;
 
     /**
      * Creates a CrappUI
@@ -24,33 +30,75 @@ public class CrappUI {
     public void run() {
         System.out.println(WELCOME_MESSAGE);
 
-        loginScreen();
+        while(!loginAttempt) {
+            loginScreen();
+        }
+
+        while(!loggedOut) {
+			mainMenu();
+		}
+    }
+
+    public void mainMenu() {
+        menuOptions(mainMenuOptions);
+			
+        int input = promptUser(mainMenuOptions.length-1);
         
+        if(input == -1) {
+            System.out.println("Not a valid command");
+        }
+
+        if(input == mainMenuOptions.length-2) {
+            System.out.println("Good bye, and have a nice day");
+            loggedOut = true;
+        }
+
+        switch(input) {
+            case(1):
+                addition();
+            case(2):
+                removal();
+            case(3):
+                search();
+        }
+    }
+
+    public void addition() {
+        menuOptions(additionOptions);
+    }
+
+    public void removal() {
+        menuOptions(removalOptions);
+    }
+
+    public void search() {
+        menuOptions(searchOptions);
     }
 
     /**
      * Prints the login/register screen
      * @return 
      */
-    public boolean loginScreen() {
-        System.out.println("1. Login");
-        System.out.println("2. Register");
+    public void loginScreen() {
+        menuOptions(loginOptions);
         int logOrReg = promptUser(2);
         if(logOrReg == 1)
         {
-            return login();
+            login();
         }
         else if(logOrReg == 2)
         {
             register();
-            return login();
+            login();
         }
-        else
-        {
-            return false;
+        else {
+            loginScreen();
         }
     }
 
+    /**
+     * Registers the user
+     */
     public void register() {
         System.out.println("Enter your First Name: ");
         String firstName = scanner.nextLine();
@@ -80,26 +128,26 @@ public class CrappUI {
      * The login for the user
      * @return Returns true if the login is successful and false otherwise
      */
-    public boolean login() {
+    public void login() {
         System.out.println("Enter your Badge Number: ");
         String badgeNumber = scanner.nextLine();
         System.out.println("Enter your Password: ");
         String password = scanner.nextLine();
         for(User user : crapp.getUsers()) {
             if(user.getBadgeNumber().equals(badgeNumber) && user.getPassword().equals(password)) {
-                return true;
+                loginAttempt = true;
             }
         }
-        return false;
+        loginAttempt = false;
     }
 
     /**
-     * Displays the main menu
+     * Displays the given menu
      */
-    public void mainMenu() {
-        System.out.println("******** MAIN MENU ********");
-        for(int i=0;i<mainMenuOptions.length;i++) {
-            System.out.println((i+1) + ". " + mainMenuOptions[i]);
+    public void menuOptions(String[] arr) {
+        System.out.println("******** "+arr[0]+" ********");
+        for(int i=1;i<arr.length;i++) {
+            System.out.println((i) + ". " + arr[i]);
         }
         System.out.println("\n");
     }
