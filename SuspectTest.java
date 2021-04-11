4bc0c35391c9424a580bf24ee8c248ec49c75392
sarahcarluccii <sarahlucci25@gmail.com>
2021-04-11 18:16:54 -0400

    suspect tests dont work):

--- SuspectTest.java
+++ SuspectTest.java
@@ -41,4 +41,54 @@ public class SuspectTest {
 		int has2 = suspectList.get(1).getUuid();
 		assertEquals(2, has2);
 	}
+
+	@Test 
+	public void paramConstructorTest() {
+		int id = 0;
+		String firstName = "0", lastName = "0", skinColor = "0", hairColor = "0", eyeColor = "0",
+		address = "0", alibi = "0";
+		int age = 0, height = 0, weight = 0;
+		ArrayList<String> tattoos = new ArrayList<String>();
+		ArrayList<String> aliases = new ArrayList<String>();
+		ArrayList<Person> associates = new ArrayList<Person>();
+		ArrayList<Crime> pastCrimes = new ArrayList<Crime>();
+		ArrayList<Evidence> evidence = new ArrayList<Evidence>();
+
+		Suspect s = new Suspect(id, firstName, lastName, age, height, weight,
+		 skinColor, hairColor, eyeColor, address, alibi);
+
+		assertEquals(id, s.getUuid());
+        assertEquals(firstName, s.getFirstName());
+        assertEquals(lastName, s.getLastName());
+        assertEquals(age, s.getAge());
+        assertEquals(height, s.getHeight());
+        assertEquals(weight, s.getWeight());
+        assertEquals(skinColor, s.getSkinColor());
+        assertEquals(eyeColor, s.getEyeColor());
+        assertEquals(address, s.getAddress());
+        assertEquals(alibi, s.getAlibi());
+		assertNotNull(s.getTattoo());
+		assertNotNull(s.getAlias());
+	}
+	@Test
+	public void addAliasTest() {
+		int id = 0;
+		String firstName = "0", lastName = "0", skinColor = "0", hairColor = "0", eyeColor = "0",
+		address = "0", alibi = "0";
+		int age = 0, height = 0, weight = 0;
+		ArrayList<String> tattoos = new ArrayList<String>();
+		ArrayList<String> aliases = new ArrayList<String>();
+		ArrayList<Person> associates = new ArrayList<Person>();
+		ArrayList<Crime> pastCrimes = new ArrayList<Crime>();
+		ArrayList<Evidence> evidence = new ArrayList<Evidence>();
+
+		Suspect s = new Suspect(id, firstName, lastName, age, height, weight,
+		 skinColor, hairColor, eyeColor, address, alibi);
+
+		String aliases1 = "test";
+		aliases.add(aliases1);
+		s.addAlias(aliases1);
+		assertEquals(aliases, s.getAlias());
+
+	}
 }
